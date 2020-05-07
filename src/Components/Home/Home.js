cfaa6aedc8f803946e7e0f6710083f5266d0e8fe
Luiz Felipe <luizzfelip00@gmail.com>
2020-05-07 15:29:46 -0300

    Home component

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Main from '../Templates/Main'
+
+
+export default props =>
+        <Main icon="home" title="Início">
+            <div className="display-4">Bem vindo!</div>
+            <hr />
+            <p className="mb-0">System developed for the porpose of learning</p>
+        </Main>
