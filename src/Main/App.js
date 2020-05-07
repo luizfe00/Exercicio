1fd11a2a45c32a66d76382f0c9b74983522a19d8
Luiz Felipe <luizzfelip00@gmail.com>
2020-05-07 15:34:23 -0300

    App Component

--- src/Main/App.js
+++ src/Main/App.js
@@ -5,6 +5,9 @@ import 'font-awesome/css/font-awesome.min.css'
 import { HashRouter } from 'react-router-dom'
 
 import Routes from './Routes'
+import Header from '../Components/Templates/Header'
+import Nav from '../Components/Templates/Nav'
+import Home from '../Components/Home/Home'
 
 export default props => 
       <HashRouter>
