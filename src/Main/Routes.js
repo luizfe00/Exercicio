45c937a3e3580951876e0adba77527e3100e199f
Luiz Felipe <luizzfelip00@gmail.com>
2020-05-10 11:16:16 -0300

    Adding User component to the /users route

--- src/Main/Routes.js
+++ src/Main/Routes.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Switch, Route, Redirect } from 'react-router'
+import User from '../Components/User/User'
+import Home from '../Components/Home/Home'
 
 
 
 
 export default props => 
         <Switch>
-            <Route exact path='/' />
-            <Route path='/users' />
+            <Route exact path='/' component={Home}/>
+            <Route path='/users' component={User}/>
         </Switch>
