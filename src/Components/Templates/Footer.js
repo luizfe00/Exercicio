fcb66c77c426b60084d12393a2104b0f64850313
Luiz Felipe <luizzfelip00@gmail.com>
2020-05-10 11:12:05 -0300

    Adding Footer

---
+++ src/Components/Templates/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Footer.css'
+
+export default props => 
+    <footer className="footer">
+        <span>
+            Luiz Felipe <i className="fa fa-address-card" />
+        </span>
+    </footer>
