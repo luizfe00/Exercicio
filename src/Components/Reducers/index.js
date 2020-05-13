cac7b6d9b277118c15c5d29122247a8925a11da7
Luiz Felipe <luizzfelip00@gmail.com>
2020-05-12 23:59:57 -0300

    rootReducer

--- src/Components/Reducers/index.js
+++ src/Components/Reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux'
 import userReducer from './userReducer'
+import errorReducer from './errorReducer'
 
 export default combineReducers({
-    users: userReducer
+    users: userReducer,
+    error: errorReducer
 })
